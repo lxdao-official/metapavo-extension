cc22153c82e6989c3b0a5c9d80f38ae320a40792
yootou <yootou@lxdao.io>
2022-08-21 10:55:43 +0800

    feat: svg

--- packages/metapavo/src/content/components/main/main.tsx
+++ packages/metapavo/src/content/components/main/main.tsx
@@ -70,33 +70,43 @@ function App() {
         </Routes>
       </div>
       {useG.showMain ? (
-        <svg
-          width="36"
-          height="36"
-          viewBox="0 0 36 36"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ position: "absolute", top: 0, left: "-36px", cursor: "pointer" }}
+        <div
           onClick={() => {
             useG.setShowMain(false);
           }}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "-36px",
+            cursor: "pointer",
+            width: "36px",
+            height: "36px",
+          }}
         >
-          <rect width="36" height="36" fill="#EFEFEF" fillOpacity="0.64" />
-          <path
-            d="M13 13L23 23"
-            stroke="#D1D0D6"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M13 23L23 13"
-            stroke="#D1D0D6"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="36" height="36" fill="#EFEFEF" fillOpacity="0.64" />
+            <path
+              d="M13 13L23 23"
+              stroke="#D1D0D6"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M13 23L23 13"
+              stroke="#D1D0D6"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       ) : null}
     </RootElement>
   );
