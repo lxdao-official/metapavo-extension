9606a4e0091379f6438b0094e9ba3431837b0d47
TOU <undefined>
2022-09-21 14:12:44 +0800

    fix: etherscan detect error

--- packages/metapavo/src/utils/recognizer/etherscan.ts
+++ packages/metapavo/src/utils/recognizer/etherscan.ts
@@ -1,10 +1,12 @@
 export async function recognizerEtherscan() {
   if (window.location.host.indexOf("etherscan.io") === -1) return;
 
-  const detailMatchResult = window.location.href.match(/etherscan\.io\/token\/([a-zA-Z0-9]*)/);
-  if (detailMatchResult && detailMatchResult[1]) {
+  const detailMatchResult = window.location.href.match(
+    /etherscan\.io\/(token|address)\/([a-zA-Z0-9]*)/,
+  );
+  if (detailMatchResult && detailMatchResult[2]) {
     return {
-      contract: detailMatchResult[1],
+      contract: detailMatchResult[2],
     };
   }
   return null;
