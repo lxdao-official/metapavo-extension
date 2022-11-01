b1ae0f6121c207c4a80ecd5a4b14ea1d30956b6f
TOU <undefined>
2022-11-01 12:11:57 +0800

    fix: add ignore

--- extensions/dashboard/_next/static/XRFfN_9InhzWGk_5IzbFQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-5ed0d06daecac46d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
