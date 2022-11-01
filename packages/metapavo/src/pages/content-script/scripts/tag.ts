9e6d49a64581173febfa890967d3071a756c2ea7
TOU <undefined>
2022-11-01 17:38:26 +0800

    fix: windows build error

--- packages/metapavo/src/pages/content-script/scripts/tag.ts
+++ packages/metapavo/src/pages/content-script/scripts/tag.ts
@@ -1,1 +1,30 @@
-export function addressTag() {}
+export function addressTag() {
+  // 遍历所有 dom 节点，判断内容是否包含以太坊地址
+  //   const doms = document.querySelectorAll('*');
+  //   const matchedElements = [];
+  //   for (let i = 0; i < doms.length; i++) {
+  //     const dom = doms[i];
+  //     const text = dom.textContent;
+  //     if (text && text.length >= 42) {
+  //       if (text.match(/0x[a-fA-F0-9]{40}/)) {
+  //         matchedElements.push(dom);
+  //         dom.classList.add('metapavo-eth-address');
+  //       }
+  //     }
+  //   }
+
+  // 遍历所有 dom 节点，判断内容是否包含以太坊地址，只判断叶子节点
+  const leafDoms = document.querySelectorAll('*:not(:has(*))');
+  const leafMatchedElements = [];
+  for (let i = 0; i < leafDoms.length; i++) {
+    const dom = leafDoms[i];
+    const text = dom.textContent;
+    if (text && text.length >= 42) {
+      if (text.match(/0x[a-fA-F0-9]{40}/)) {
+        leafMatchedElements.push(dom);
+        dom.classList.add('metapavo-eth-address');
+      }
+    }
+  }
+}
+addressTag();
