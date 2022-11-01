47c2e09de9829db2c88860a2643fc0d32ec393b5
TOU <undefined>
2022-11-01 12:11:16 +0800

    feat: add dashboard app, and use turbopack

---
+++ extensions/dashboard/_next/static/XRFfN_9InhzWGk_5IzbFQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-5ed0d06daecac46d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
