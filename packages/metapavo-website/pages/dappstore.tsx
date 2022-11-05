e90902068415c3fc2cb85ca26b790fcc3883b02b
dissey <39985686+dissey@users.noreply.github.com>
2022-11-05 17:49:34 +0800

    feat: add page

---
+++ packages/metapavo-website/pages/dappstore.tsx
@@ -0,0 +1,9 @@
+import LayoutAdmin from '../components/LayoutAdmin';
+
+export default function Dappstore() {
+  return (
+    <LayoutAdmin title="admin" description={'admin'}>
+      <div>123</div>
+    </LayoutAdmin>
+  );
+}
